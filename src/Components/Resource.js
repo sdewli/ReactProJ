2f3e0e05b3028ee0263211ca4c5cafd6fd5509f3
sdewli <saurabhdewli@gmail.com>
2022-09-20 13:36:58 +0530

    Update Resource.js

--- src/Components/Resource.js
+++ src/Components/Resource.js
@@ -1,8 +1,8 @@
 import React from "react";
-import {useParams} from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
 export default function Resource({ data }) {
-    const {id} = useParams();
+  const { id } = useParams();
   return (
     <div>
       {data &&
@@ -10,18 +10,19 @@ export default function Resource({ data }) {
           if (resource.id === id)
             return (
               <>
-              <div className="Resource">
-                <div className="div2">
-                  {<img className="img2" src={resource.icon_url} />}
-                  <span className="title">{resource.title}</span>
-                  <span className="">{resource.id}</span>
-                  <span className="category">{resource.category}</span>
-                </div>
-                <div>
-                  <span className="link"><a href={resource.link} >{resource.link} </a></span>
-                  
-                  <span className="description">{resource.description}</span>
-                </div>
+                <div className="Resource">
+                  <div className="div2">
+                    {<img className="img2" src={resource.icon_url} />}
+                    <span className="title">{resource.title}</span>
+                    <span className="">{resource.id}</span>
+                    <span className="category">{resource.category}</span>
+                  </div>
+                  <div>
+                    <span className="link">
+                      <a href={resource.link}>{resource.link} </a>
+                    </span>
+                    <span className="description">{resource.description}</span>
+                  </div>
                 </div>
               </>
             );
@@ -29,5 +30,3 @@ export default function Resource({ data }) {
     </div>
   );
 }
-
-
